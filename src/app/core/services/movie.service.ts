831d9dc669bc62ba3b3ee332a65a533f553e0f70
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-05-07 16:25:55 -0500

    feat: implementar movie cards y sliders usando control flow @for

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -3,14 +3,17 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { MovieResponse } from '../models/movie.model';
 
-@Injectable({ providedIn: 'root' }) // Disponible en toda la app
+@Injectable({ providedIn: 'root' })
 export class MovieService {
-  private http = inject(HttpClient); // Inyectamos el motor HTTP
+  private http = inject(HttpClient);
   private apiUrl = environment.baseUrl;
 
   getTrendingMovies() {
-    // Retornamos un Observable (una promesa de que llegarán datos)
-    return this.http.get<MovieResponse>
-      (`${this.apiUrl}/trending/movie/day`);
+    return this.http.get<MovieResponse>(`${this.apiUrl}/trending/movie/day`);
+  }
+
+  // NUEVO MÉTODO
+  getPopularMovies() {
+    return this.http.get<MovieResponse>(`${this.apiUrl}/movie/popular`);
   }
 }
