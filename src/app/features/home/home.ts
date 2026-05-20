d09bf574b1beefefd4b6938e3a829759f0a978f5
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-05-20 14:01:14 -0500

    feat: implementar scroll infinito usando intersection observer en  el home

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,24 +1,35 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+// 1. Necesitamos herramientas para interactuar con el DOM de forma segura
+import { AfterViewInit, Component, ElementRef, inject, OnInit, PLATFORM_ID, signal, ViewChild } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { MovieService } from '../../core/services/movie.service';
 import { Hero } from './components/hero/hero';
 import { MovieSlider } from '../../shared/components/movie-slider/movie-slider';
+import { MovieCard } from '../../shared/components/movie-card/movie-card';
 import { Movie } from '../../core/models/movie.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Hero, MovieSlider],
+  imports: [Hero, MovieSlider, MovieCard],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 
-export class Home implements OnInit {
+export class Home implements OnInit, AfterViewInit {
   private movieService = inject(MovieService);
+  private platformId = inject(PLATFORM_ID);
+
+  // 2. Marcamos un elemento del HTML para observarlo
+  @ViewChild('infiniteAnchor') infiniteAnchor!: ElementRef;
 
   // Declaramos nuestras Signals para almacenar el estado de forma reactiva
   featuredMovie = signal<Movie | null>(null);
   trendingMovies = signal<Movie[]>([]);
   popularMovies = signal<Movie[]>([]);
+
+  catalogMovies = signal<Movie[]>([]);
+  currentPage = signal(1);
+  isFetchingNextPage = signal(false);
 
   ngOnInit(): void {
     // 1. Pedimos las tendencias
@@ -35,6 +46,36 @@ export class Home implements OnInit {
     this.movieService.getPopularMovies().subscribe({
       next: (data) => {
         this.popularMovies.set(data.results); // Guardamos la lista de populares
+      }
+    });
+  }
+
+  ngAfterViewInit(): void {
+    // 3. Solo configuramos el observador en el navegador (SSR Safety)
+    if (isPlatformBrowser(this.platformId)) {
+      this.initInfiniteScroll();
+    }
+  }
+
+  private initInfiniteScroll(): void {
+    const observer = new IntersectionObserver((entries) => {
+      // 4. Si el ancla entra en el campo de visión y no estamos cargando...
+      if (entries[0].isIntersecting && !this.isFetchingNextPage()) {
+        this.loadMoreMovies();
+      }
+    }, { rootMargin: '200px' }); // 'rootMargin' permite cargar 200px antes de llegar al final
+
+    observer.observe(this.infiniteAnchor.nativeElement);
+  }
+
+  loadMoreMovies(): void {
+    this.isFetchingNextPage.set(true);
+    this.movieService.getPopularMovies(this.currentPage()).subscribe({
+      next: (data) => {
+        // 5. Inmutabilidad: Concatenamos los resultados usando el operador spread [...]
+        this.catalogMovies.set([...this.catalogMovies(), ...data.results]);
+        this.currentPage.update(p => p + 1);
+        this.isFetchingNextPage.set(false);
       }
     });
   }
