d0fb3945830109f6cfde043cccf190391e1f8469
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-05-28 14:52:41 -0500

    feat: setup PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideServiceWorker } from '@angular/service-worker';
 import { apiInterceptor } from './core/interceptors/api.interceptor';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { routes } from './app.routes';
@@ -11,6 +12,11 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withFetch(), // Habilita el uso de la API Fetch moderna (más rápida y compatible con SSR)
       withInterceptors([apiInterceptor, errorInterceptor]) // ¡Añadido aquí!
-    )
+    ),
+    // NUEVO: Registrar el Service Worker
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
